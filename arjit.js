57e590c74a12ee758a904e475ad70ebce43535be
shubham03 <shubhams2607@gmail.com>
2024-01-05 17:56:02 +0530

    make changes

--- arjit.js
+++ arjit.js
@@ -1,19 +1,20 @@
-const sound = document.getElementById("sound");
-        const icon = document.getElementById("icon");
-
-        icon.onclick =  function()
-        {
-            if(sound.paused){
-                sound.play();
-                icon.src="stop-button.png";
-                // If paused then play the
-                // music and change the image
-            }
-            else{
-                sound.pause();
-                icon.src="im\play_button.png";
-            }
-        }
+console.log("hello");
         
+let songIndex = 0;
+let audioElement = new Audio('musics\Tum Hi Ho.mp3');
+let songItem = Array.from(document.getElementsByClassName("songItems"))
 
-        
+let songs=[
+    {songName:"Tum hi Ho" , filePath:"musics\Tum Hi Ho.mp3" },
+    {songName:"Kabhi Jo Badal Barse",filePath:"musics\Kabhi Jo Baadal Barse.mp3"},
+    {songName:"Tera Yaar Hoon Main",filePath:""},
+    {songName:" Milne Hai Mujhse AAyi", filePath:"musics\Milne Hai Mujhse Aayi.mp3"},
+    {songName:"Muskrana", filePath:"musics\Muskurane.mp3"},
+    {songName:"Agar Tum Saath Ho", filePath:" "},
+    {songName:" ", filePath:" "}
+]
+
+songItem.forEach((elements,i)=>{
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+
+}) 
