321fc99b6e509ec4fc249e8248faf70ca8e31c0f
shubham03 <shubhams2607@gmail.com>
2024-03-20 12:14:10 +0530

    y

--- arjit.js
+++ arjit.js
@@ -1,19 +1,35 @@
-console.log("hello");
-        
-let songIndex = 0;
-let audioElement = new Audio('musics\Tum Hi Ho.mp3');
-let songItem = Array.from(document.getElementsByClassName("songItems"))
+let buttons = document.querySelectorAll("button");
+let currentAudio;
+// let pauseSong = document.querySelector(".pause");
+// pauseSong.addEventListener("click",(){
+//     currentAudio.pause();
+// })
 
-let songs=[
-    {songName:"Tum hi Ho" , filePath:"musics\Tum Hi Ho.mp3" },
-    {songName:"Kabhi Jo Badal Barse",filePath:"musics\Kabhi Jo Baadal Barse.mp3"},
-    {songName:"Tera Yaar Hoon Main",filePath:""},
-    {songName:" Milne Hai Mujhse AAyi", filePath:"musics\Milne Hai Mujhse Aayi.mp3"},
-    {songName:"Muskrana", filePath:"musics\Muskurane.mp3"},
-    {songName:"Agar Tum Saath Ho", filePath:" "},
-    {songName:" ", filePath:" "}
-]
+for (let val of buttons) {
+    val.addEventListener("click", function () {
+        // If there is a currently playing audio, pause it
+        if (currentAudio) {
+            currentAudio.pause();
+        }
 
-songItem.forEach((elements,i)=>{
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
-}) 
+        let buttonClicked = this.innerHTML;
+
+        switch (buttonClicked) {
+            case "Tum Hi Ho": currentAudio = new Audio("musics/Tum Hi Ho.mp3");
+                break;
+            case "Kabhi Jo Badal Barse": currentAudio = new Audio("musics/Kabhi Jo Baadal Barse.mp3");
+                break;
+            case "Muskrana": currentAudio = new Audio("musics/Muskurane.mp3");
+                break;
+            case "Milne Hai Mujhse": currentAudio = new Audio("musics/Milne Hai Mujhse Aayi.mp3");
+                break;
+        }
+
+        // Play the new audio
+        if (currentAudio) {
+            currentAudio.play();
+        }
+    });
+}
+
+
